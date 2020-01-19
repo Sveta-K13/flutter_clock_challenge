bb105d0e2a8ed6ffcc04ad12caf218f7b04ea8b9
Sveta Krivosheeva <sveta@effective.band>
2020-01-19 23:49:31 +0600

    add lines

--- lib/analog_clock.dart
+++ lib/analog_clock.dart
@@ -4,21 +4,19 @@
 
 import 'dart:async';
 
+import 'package:flutter/services.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
 import 'package:intl/intl.dart';
-import 'package:vector_math/vector_math_64.dart' show radians;
-
-import 'container_hand.dart';
 import 'drawn_hand.dart';
 
 /// Total distance traveled by a second or a minute hand, each second or minute,
 /// respectively.
-final radiansPerTick = radians(360 / 60);
+// final radiansPerTick = radians(360 / 60);
 
 /// Total distance traveled by an hour hand, each hour, in radians.
-final radiansPerHour = radians(360 / 12);
+// final radiansPerHour = radians(360 / 12);
 
 /// A basic analog clock.
 ///
@@ -47,6 +45,11 @@ class _AnalogClockState extends State<AnalogClock> {
     // Set the initial values.
     _updateTime();
     _updateModel();
+    // hack for horizontal orientation
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft
+    ]);
   }
 
   @override
@@ -113,7 +116,7 @@ class _AnalogClockState extends State<AnalogClock> {
           );
 
     final time = DateFormat.Hms().format(DateTime.now());
-    // print(_now.second / 60);
+
     return Semantics.fromProperties(
       properties: SemanticsProperties(
         label: 'Analog clock with time $time',
@@ -123,6 +126,38 @@ class _AnalogClockState extends State<AnalogClock> {
         color: customTheme.backgroundColor,
         child: Stack(
           children: [
+            Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  for (var i = 1; i < 13; i++)
+                  Expanded(
+                    flex: 1,
+                    child: 
+                    Container(
+                      // color: Colors.yellow,
+                      decoration: BoxDecoration(
+                        border: Border(
+                          right: BorderSide(
+                            color: Color(0xffF6F4F3),
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Text(i < 10 ? '0$i' : i.toString(), textAlign: TextAlign.center)
+                          ,
+                          Text((i * 5) < 10 ?'0${i * 5}' : (i * 5).toString()),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+            ),
             DrawnHand(
               color: Color(0xFFF3F0FA),
               colorFill: Color(0xFFB393FF),

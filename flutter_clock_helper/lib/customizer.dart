803dbdcdf51335b7342a0a13bcd1a35d00555bcb
Sveta Krivosheeva <sveta@effective.band>
2020-01-20 12:58:39 +0600

    cut unneeded

--- flutter_clock_helper/lib/customizer.dart
+++ flutter_clock_helper/lib/customizer.dart
@@ -177,12 +177,7 @@ class _ClockCustomizerState extends State<ClockCustomizer> {
       child: AspectRatio(
         aspectRatio: 5 / 3,
         child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              width: 2,
-              color: Theme.of(context).unselectedWidgetColor,
-            ),
-          ),
+          padding: EdgeInsets.symmetric(horizontal: 30),
           child: widget._clock(_model),
         ),
       ),
